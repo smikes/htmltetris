08a7728a4a9161b5754ceb792650d584fad2df54
Sam Mikes <smikes@cubane.com>
2014-06-15 15:20:20 +0100

    add smoke tests

--- test/startup.js
+++ test/startup.js
@@ -4,5 +4,15 @@ describe('Tetris', function () {
     it("should load without throwing under node", function () {
         var TETRIS = require('../lib/tetris.js');
 
+        TETRIS.should.be.a.Function;
+    });
+
+    it("should not crash if called with dummies construction", function () {
+        var TETRIS = require('../lib/tetris.js');
+        
+        var doc = {}, win = {};
+
+        var t = new TETRIS(doc, win);
+   
     });
 });
