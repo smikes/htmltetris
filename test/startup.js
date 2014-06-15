f00e86531591c45328c5fd28945ca3110857a2ff
Sam Mikes <smikes@cubane.com>
2014-06-15 15:16:53 +0100

    drop expectation about what returns from require()

--- test/startup.js
+++ test/startup.js
@@ -4,6 +4,5 @@ describe('Tetris', function () {
     it("should load without throwing under node", function () {
         var TETRIS = require('../lib/tetris.js');
 
-        (TETRIS).should.be.a.Function;
     });
 });
